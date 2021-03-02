c6d43ada67f5e40618b10951373850d61e09b773
fengyulin <fengyulin@bytedance.com>
2021-03-03 01:44:57 +0800

    Update index.tsx

--- src/Containers/BreakoutClone/index.tsx
+++ src/Containers/BreakoutClone/index.tsx
@@ -1,16 +1,19 @@
-import React, { FC, useRef, useEffect } from 'react'
+import React, { FC, useRef, useEffect, useState } from 'react'
 import { Page } from '../../Components/Styled'
 import * as THREE from 'three'
 
 const BreakoutClone: FC = () => {
   const containerRef = useRef<HTMLDivElement | null>(null)
   const sceneRef = useRef<THREE.Scene>()
+  const renderRef = useRef<THREE.WebGLRenderer>()
+  const frameIdRef = useRef<number | undefined>()
+  const cameraRef = useRef<THREE.Camera>()
 
   const initScene = () => {
     if (!containerRef.current) {
       return
     }
-    console.log(containerRef.current)
+
     const container = containerRef.current
     const { clientHeight, clientWidth } = container
 
@@ -18,13 +21,43 @@ const BreakoutClone: FC = () => {
 
     const renderer = new THREE.WebGLRenderer({ antialias: true })
     renderer.setSize(clientWidth, clientHeight)
+    renderer.setClearColor(new THREE.Color(0xffffff))
+    renderRef.current = renderer
     containerRef.current.appendChild(renderer.domElement)
   }
 
-  const initCamera = () => {}
+  const initCamera = () => {
+    const container = containerRef.current!
+    const { clientHeight, clientWidth } = container
+
+    const camera = new THREE.PerspectiveCamera(90, clientWidth / clientHeight, 0.1, 1000)
+
+    camera.position.set(30, 5, 70)
+    camera.lookAt(sceneRef.current!.position)
+
+    cameraRef.current = camera
+  }
+
+  const initLight = () => {
+    const ambientlight = new THREE.AmbientLight(0xffffff)
+    sceneRef.current!.add(ambientlight)
+  }
+
+  const startRender = () => {
+    console.log(frameIdRef.current, !frameIdRef.current)
+    if (!frameIdRef.current) {
+      frameIdRef.current = requestAnimationFrame(() => {
+        renderRef.current?.render(sceneRef.current!, cameraRef.current!)
+      })
+    }
+  }
 
   useEffect(() => {
     initScene()
+    initCamera()
+    initLight()
+
+    startRender()
   }, [])
 
   return <Page ref={containerRef}></Page>
